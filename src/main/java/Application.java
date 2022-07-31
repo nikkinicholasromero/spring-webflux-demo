c807281206c231916016371b270da94a278c7433
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2022-08-01 05:51:31 +0800

    Multiple routes

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,22 +1,42 @@
-import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 import reactor.netty.DisposableChannel;
 import reactor.netty.http.server.HttpServer;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
 public class Application {
     public static void main(String[] args) {
-        HttpHandler httpHandler = RouterFunctions.toHttpHandler(
-                new MainRouter().route()
-        );
-
-        ReactorHttpHandlerAdapter reactorHttpHandler = new ReactorHttpHandlerAdapter(httpHandler);
-
         HttpServer.create()
                 .port(8080)
-                .handle(reactorHttpHandler)
+                .handle(new ReactorHttpHandlerAdapter(RouterFunctions.toHttpHandler(Application.route())))
                 .bind()
                 .flatMap(DisposableChannel::onDispose)
                 .block();
+    }
+
+    public static RouterFunction<ServerResponse> route() {
+        return RouterFunctions
+                .route(GET("/world").and(accept(MediaType.APPLICATION_JSON)), Application::helloWorld)
+                .andRoute(GET("/earth").and(accept(MediaType.APPLICATION_JSON)), Application::helloEarth);
+    }
+
+    public static Mono<ServerResponse> helloWorld(ServerRequest request) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue("Hello, World"));
+    }
+
+    public static Mono<ServerResponse> helloEarth(ServerRequest request) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue("Hello, Earth"));
     }
 }
